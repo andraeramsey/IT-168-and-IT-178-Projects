717ecce5206c79c9eaf5cf91c854d11e8d5d6f7a
andraeramsey <andraeramsey@gmail.com>
2019-04-24 00:52:45 -0500

    Add files via upload

---
+++ Lab02/Lab02/src/edi/ilstu/DistanceCalculator.java
@@ -0,0 +1,31 @@
+/*
+ * File name: FirstClass.java
+ * Programmer: Andrae Ramsey
+ * ULID: arrams1
+ * Date: Aug 27, 2015
+ *
+ * Class: IT 168
+ * Lecture Section: 19
+ * Lecture Instructor: Schaefer
+ * Lab Section: 21
+ * Lab Instructor: Kora
+ */
+/**
+ * <A program calculating speed and time>
+ * @author Andrae Ramsey
+ *
+ */
+package edi.ilstu;
+
+public class DistanceCalculator
+{public static void main (String[] args)
+	{int speed =20;
+			int time =10;
+			int distance = speed*time;
+			System.out.println(distance);
+		
+	
+	}
+}
+
+
---
+++ Lab02/Lab02/src/edi/ilstu/FirstClass.java
@@ -0,0 +1,30 @@
+/*
+ * File name: FirstClass.java
+ * Programmer: Andrae Ramsey
+ * ULID: arrams1
+ * Date: Aug 27, 2015
+ *
+ * Class: IT 168
+ * Lecture Section: 19
+ * Lecture Instructor: Schaefer
+ * Lab Section: 21
+ * Lab Instructor: Kora
+ */
+package edi.ilstu;
+
+/**
+ * <A first program rearranging statements>
+ * @author Andrae Ramsey
+ *
+ */
+public class FirstClass
+{public static void main (String[] args)
+{
+
+	{System.out.println("Welcoime to Java programming!");
+	System.out.println("Java rules!");
+	System.out.println("My name is Andrae Ramsey");
+	System.out.println("Success!\n");
+	System.out.print("\nGoodbye!");}
+	}
+}
